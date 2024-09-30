b3abb14ba30a53377d7c6b334e3a51bfca73c528
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2024-09-30 15:52:43 -0300

    exemplo conexão postgresq

---
+++ phppostgress/index.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = 'localhost'; // ou o endereço do servidor
+$port = '5432'; // porta padrão do PostgreSQL
+$dbname = 'livro'; // nome do seu banco de dados
+$user = 'postgres'; // seu usuário do banco de dados
+$password = 'postgres'; // sua senha do banco de dados
+
+try {
+    // Cria a conexão usando PDO
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password);
+
+    // Define o modo de erro do PDO para exceções
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conexão bem-sucedida ao PostgreSQL!<br>";
+    echo "Acessando: $dbname";
+
+    $stmt = $pdo->prepare("SELECT * FROM livro");
+    $stmt->execute();
+    echo "<br><ul>";
+    foreach ($stmt as $linha) {
+      
+      echo "<li>";
+      echo "Codigo".$linha["codigo"]." Livro: ".$linha["titulo"]." autor: ".$linha["autor"]." ano: ".$linha["ano"];
+      echo "</li>";
+    }
+    echo "</ul>";
+
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
